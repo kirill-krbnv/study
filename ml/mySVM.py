1ca48dd3455c3777a8a44200eeb86bdfe0412c62
kirillkarabanov <kirillkrbnv@gmail.com>
2026-08-19 13:17:45 +0300

    my svm last commit

--- ml/mySVM.py
+++ ml/mySVM.py
@@ -1,5 +1,9 @@
 import numpy as np
 import pandas as pd
+import random
+
+from sympy.utilities.iterables import iterable
+
 
 class MySVM:
     def __init__(
@@ -7,12 +11,18 @@ class MySVM:
             n_iter=10,
             learning_rate=0.001,
             weights = None,
-            b = None
+            b = None,
+            C: float=1,
+            sgd_sample=None,
+            random_state: int=42
     ) -> None:
         self.n_iter = n_iter
         self.learning_rate = learning_rate
         self.weights = weights
         self.b = b
+        self.c = C
+        self.sgd_sample = sgd_sample
+        self.random_state = random_state
 
     def __repr__(self) -> str:
         return f'MySVM class: n_iter={self.n_iter}, learning_rate={self.learning_rate}'
@@ -20,12 +30,26 @@ class MySVM:
     def _compute_full_loss(self, X: pd.DataFrame, y: pd.Series):
 
         margins = 1 - y * (X.to_numpy() @ self.weights + self.b)
-        classification_error = np.maximum(0, margins).mean()
+        classification_error = self.c * np.maximum(0, margins).mean()
         margin_error = np.sum(self.weights**2)
 
         return margin_error + classification_error
 
+    def sgd_batches(self, X, y):
+
+        if isinstance(self.sgd_sample, int):
+            batch_size = self.sgd_sample
+        else:
+            batch_size = round(self.sgd_sample * len(X))
+        batch_rows_ids = random.sample(range(X.shape[0]), batch_size)
+        X_batch = pd.DataFrame(X.iloc[batch_rows_ids])
+        y_batch = pd.Series(y.iloc[batch_rows_ids])
+
+        return X_batch, y_batch
+
     def fit(self, X: pd.DataFrame, y: pd.Series, verbose=None):
+
+        random.seed(self.random_state)
 
         if (y==0).any():
             y = y.replace(0, -1)
@@ -40,17 +64,23 @@ class MySVM:
 
         for epoch in range(1, self.n_iter + 1):
 
-            for index, x_row in X.iterrows():
+            if self.sgd_sample is not None:
+                X_sample, y_sample = self.sgd_batches(X, y)
+                iterable_X = X_sample.iterrows()
+            else:
+                iterable_X = X.iterrows()
 
-                y_true = y[index]
+            for index, x_row in iterable_X:
+
                 x_vec = x_row.to_numpy()
+                y_true = y[index]
 
                 if y_true * (self.weights @ x_vec + self.b) >= 1:
                     grad_w = 2 * self.weights
                     grad_b = 0
                 else:
-                    grad_w = 2 * self.weights - y_true * x_vec
-                    grad_b = -y_true
+                    grad_w = 2 * self.weights - self.c * y_true * x_vec
+                    grad_b = -self.c * y_true
 
                 self.weights = self.weights - alpha * grad_w
                 self.b = self.b - alpha * grad_b
@@ -68,22 +98,24 @@ class MySVM:
             y[y == -1] = 0
         return y
 
-from sklearn.datasets import make_classification
-X_train, y_train = make_classification(
-    n_samples=100,
-    n_classes=2,
-    n_features=4,
-    flip_y=0.05,
-    random_state=47
-)
 
-X_train = pd.DataFrame(X_train)
-y_train = pd.Series(y_train)
-
-model = MySVM(100, 0.002)
-model.fit(X=X_train, y=y_train, verbose=5)
-print(model)
-
-print(y_train)
-y_pred = model.predict(X_train)
-print((y_pred == y_train).mean())
+# _________________________________________________________________________
+# from sklearn.datasets import make_classification
+# X_train, y_train = make_classification(
+#     n_samples=100,
+#     n_classes=2,
+#     n_features=4,
+#     flip_y=0.05,
+#     random_state=47
+# )
+#
+# X_train = pd.DataFrame(X_train)
+# y_train = pd.Series(y_train)
+#
+# model = MySVM(100, 0.002, sgd_sample = 0.2)
+# model.fit(X=X_train, y=y_train, verbose=5)
+# print(model)
+#
+# print(y_train)
+# y_pred = model.predict(X_train)
+# print((y_pred == y_train).mean())
